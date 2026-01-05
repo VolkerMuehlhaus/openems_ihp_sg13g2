ccf62b439f82199b7d64e3f95e301e220e233f32
EngGhaith <ghaithsabagh.1998@gmail.com>
2026-01-05 09:28:22 +0100

    Update run_line_viaport_fielddump.py

--- workflow/run_line_viaport_fielddump.py
+++ workflow/run_line_viaport_fielddump.py
@@ -137,7 +137,7 @@ FDTD = simulation_setup.setupSimulation (excite_ports,
 
 
 
-sub1_data_path = simulation_setup.runSimulation (excite_ports, FDTD, sim_path, model_basename, preview_only, postprocess_only)
+sub1_data_path = simulation_setup.runSimulation (excite_ports, FDTD, sim_path, model_basename, preview_only, postprocess_only, preview_first_excitation)
 
 
 ########## evaluation of results with composite GSG ports ###########
