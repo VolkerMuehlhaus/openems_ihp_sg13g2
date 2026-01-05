3e3828082fa57e93ee63304691887014963731a6
EngGhaith <ghaithsabagh.1998@gmail.com>
2026-01-05 09:25:49 +0100

    Update run_inductor_2port.py

--- workflow/run_inductor_2port.py
+++ workflow/run_inductor_2port.py
@@ -118,7 +118,7 @@ for excite_ports in [[1],[2]]:  # list of ports that are excited one after anoth
                                              unit, 
                                              xy_mesh_function=util_meshlines.create_xy_mesh_from_polygons)
 
-    data_paths.append(simulation_setup.runSimulation (excite_ports, FDTD, sim_path, model_basename, preview_only, postprocess_only))
+    data_paths.append(simulation_setup.runSimulation (excite_ports, FDTD, sim_path, model_basename, preview_only, postprocess_only, preview_first_excitation))
 
 
 if preview_only==False:
